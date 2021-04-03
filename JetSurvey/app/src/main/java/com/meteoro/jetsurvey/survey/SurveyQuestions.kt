8712126f6b4adabb559bb035b237c94ac310e78b
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2021-04-03 15:38:56 -0300

    Answer slider choice

--- JetSurvey/app/src/main/java/com/meteoro/jetsurvey/survey/SurveyQuestions.kt
+++ JetSurvey/app/src/main/java/com/meteoro/jetsurvey/survey/SurveyQuestions.kt
@@ -1,6 +1,8 @@
 package com.meteoro.jetsurvey.survey
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -8,20 +10,30 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material.Button
 import androidx.compose.material.Checkbox
 import androidx.compose.material.CheckboxDefaults
 import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Icon
 import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.RadioButton
 import androidx.compose.material.RadioButtonDefaults
+import androidx.compose.material.Slider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddAPhoto
+import androidx.compose.material.icons.filled.SwapHoriz
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
@@ -30,8 +42,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.google.accompanist.coil.CoilImage
+import com.meteoro.jetsurvey.R
+import com.meteoro.jetsurvey.ui.theme.JetSurveyTheme
 
 @Composable
 fun Question(
@@ -101,8 +118,19 @@ fun Question(
                     },
                     modifier = Modifier.fillMaxWidth()
                 )
-                is PossibleAnswer.Action -> Text(text = "Action")
-                is PossibleAnswer.Slider -> Text(text = "Slider")
+                is PossibleAnswer.Action -> ActionQuestion(
+                    questionId = question.id,
+                    possibleAnswer = question.answer,
+                    answer = answer as Answer.Action?,
+                    onAction = onAction,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                is PossibleAnswer.Slider -> SliderQuestion(
+                    possibleAnswer = question.answer,
+                    answer = answer as Answer.Slider?,
+                    onAnswerSelected = { onAnswer(Answer.Slider(it)) },
+                    modifier = Modifier.fillMaxWidth()
+                )
             }
         }
     }
@@ -217,5 +245,182 @@ private fun MultipleChoiceQuestion(
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun ActionQuestion(
+    questionId: Int,
+    possibleAnswer: PossibleAnswer.Action,
+    answer: Answer.Action?,
+    onAction: (Int, SurveyActionType) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    when (possibleAnswer.actionType) {
+        SurveyActionType.PICK_DATE -> {
+            DateQuestion(
+                questionId = questionId,
+                answerLabel = possibleAnswer.label,
+                answer = answer,
+                onAction = onAction,
+                modifier = modifier
+            )
+        }
+        SurveyActionType.TAKE_PHOTO -> {
+            PhotoQuestion(
+                questionId = questionId,
+                answer = answer,
+                onAction = onAction,
+                modifier = modifier
+            )
+        }
+        SurveyActionType.SELECT_CONTACT -> TODO()
+    }
+}
+
+@Composable
+private fun DateQuestion(
+    questionId: Int,
+    @StringRes answerLabel: Int,
+    answer: Answer.Action?,
+    onAction: (Int, SurveyActionType) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = { onAction(questionId, SurveyActionType.PICK_DATE) },
+        modifier = modifier.padding(vertical = 20.dp)
+    ) {
+        Text(text = stringResource(id = answerLabel))
+    }
+
+    if (answer != null && answer.result is SurveyActionResult.Date) {
+        Text(
+            text = stringResource(id = R.string.selected_date, answer.result.date),
+            style = MaterialTheme.typography.h4,
+            modifier = Modifier.padding(vertical = 20.dp)
+        )
+    }
+}
+
+@Composable
+private fun PhotoQuestion(
+    questionId: Int,
+    answer: Answer.Action?,
+    onAction: (Int, SurveyActionType) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val resource = if (answer != null) {
+        Icons.Filled.SwapHoriz
+    } else {
+        Icons.Filled.AddAPhoto
+    }
+    OutlinedButton(
+        onClick = { onAction(questionId, SurveyActionType.TAKE_PHOTO) },
+        modifier = modifier,
+        contentPadding = PaddingValues()
+    ) {
+        Column {
+            if (answer != null && answer.result is SurveyActionResult.Photo) {
+                CoilImage(
+                    data = answer.result.uri,
+                    modifier = Modifier.fillMaxSize(),
+                    fadeIn = true,
+                    contentDescription = null
+                )
+            } else {
+                PhotoDefaultImage(modifier = Modifier.padding(horizontal = 86.dp, vertical = 74.dp))
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentSize(Alignment.BottomCenter)
+                    .padding(vertical = 26.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = resource, contentDescription = null)
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = stringResource(
+                        id = if (answer != null) {
+                            R.string.retake_photo
+                        } else {
+                            R.string.add_photo
+                        }
+                    )
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun PhotoDefaultImage(
+    modifier: Modifier = Modifier,
+    lightTheme: Boolean = MaterialTheme.colors.isLight
+) {
+    val assetId = if (lightTheme) {
+        R.drawable.ic_selfie_light
+    } else {
+        R.drawable.ic_selfie_dark
+    }
+    Image(
+        painter = painterResource(id = assetId),
+        modifier = modifier,
+        contentDescription = null
+    )
+}
+
+@Composable
+private fun SliderQuestion(
+    possibleAnswer: PossibleAnswer.Slider,
+    answer: Answer.Slider?,
+    onAnswerSelected: (Float) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var sliderPosition by remember {
+        mutableStateOf(answer?.answerValue ?: possibleAnswer.defaultValue)
+    }
+    Row(modifier = modifier) {
+        Text(
+            text = stringResource(id = possibleAnswer.startText),
+            modifier = Modifier.align(Alignment.CenterVertically)
+        )
+        Slider(
+            value = sliderPosition,
+            onValueChange = {
+                sliderPosition = it
+                onAnswerSelected(it)
+            },
+            valueRange = possibleAnswer.range,
+            steps = possibleAnswer.steps,
+            modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 16.dp)
+        )
+        Text(
+            text = stringResource(id = possibleAnswer.endText),
+            modifier = Modifier.align(Alignment.CenterVertically)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun QuestionPreview() {
+    val question = Question(
+        id = 2,
+        questionText = R.string.pick_superhero,
+        answer = PossibleAnswer.SingleChoice(
+            optionsStringRes = listOf(
+                R.string.spiderman,
+                R.string.ironman,
+                R.string.unikitty,
+                R.string.captain_planet
+            )
+        ),
+        description = R.string.select_one
+    )
+    JetSurveyTheme {
+        Question(question = question, answer = null, onAnswer = { }, onAction = { _, _ -> })
     }
 }
